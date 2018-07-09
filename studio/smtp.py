23d46bd7399cc7f1f7340f4026b0706d10502858
zhangks93 <zhangks93@outlook.com>
2018-07-09 22:33:02 +0800

    delete models

--- studio/smtp.py
+++ studio/smtp.py
@@ -1,10 +1,15 @@
-
-import os  
+import os,sys, django
+sys.path.append('.../aqua_studio/')
+os.environ['DJANGO_SETTINGS_MODULE'] ='aqua.settings'
+django.setup()
+from studio.models import contamination
+  
 import socket  
 import time  
 import smtplib    
 from email.mime.text import MIMEText  
-import pandas
+import pandas as pd
+import datetime
 mailto_list=["771768137@qq.com"]   
 mail_host="smtp.163.com"  #设置服务器  
 mail_user="13377875048"    #用户名  
@@ -28,14 +33,17 @@ def send_mail(to_list,sub,content):  #to_list：收件人；sub：主题；conte
         print ('error')
        
 if __name__ == '__main__':  
-
-      excel_path = './media/data.xlsx'
-      df = pandas.read_excel(excel_path,  sheet_name='Sheet1')  
-      old_width = pandas.get_option('display.max_colwidth')  
-      pandas.set_option('display.max_colwidth', -1)  
-      df.to_html('./media/data.html',escape=False,index=False,sparsify=True,border=0,index_names=False,header=True)
+      
+      try:
+        today=datetime.date.today()
+        contamination_today=contamination.objects.get(date=today)
+      except:
+        contamination_today=contamination.objects.get(date=datetime.date(2018,1,1))
+      data=pd.DataFrame([['/',contamination_today.A_pH,contamination_today.O1_pH,contamination_today.O2_pH,contamination_today.O3_pH,contamination_today.O4_pH,'/'],['/','/',contamination_today.O1_Do,contamination_today.O2_Do,contamination_today.O3_Do,contamination_today.O4_Do,'/'],[contamination_today.If_COD,contamination_today.A_COD,contamination_today.O1_COD,contamination_today.O2_COD,contamination_today.O3_COD,contamination_today.O4_COD,contamination_today.Ef_COD],[contamination_today.If_COD,contamination_today.A_COD,contamination_today.O1_COD,contamination_today.O2_COD,contamination_today.O3_COD,contamination_today.O4_COD,contamination_today.Ef_COD],[contamination_today.If_COD,contamination_today.A_COD,contamination_today.O1_COD,contamination_today.O2_COD,contamination_today.O3_COD,contamination_today.O4_COD,contamination_today.Ef_COD],[contamination_today.If_COD,contamination_today.A_COD,contamination_today.O1_COD,contamination_today.O2_COD,contamination_today.O3_COD,contamination_today.O4_COD,contamination_today.Ef_COD]],columns=['Inflluent','A','1#','2#','3#','4#','Effluent'],index=['pH','DO(mg/L)','COD(mg/L)','Ammonia(mg/L)','Nitrite(mg/L)','Nitrate(mg/L)'])
+      
+      df.to_html('../media/data.html',escape=False,index=False,sparsify=True,border=0,index_names=False,header=True)
       pandas.set_option('display.max_colwidth', old_width) 
-      htmlf=open('./media/data.html','r',encoding="utf-8")
+      htmlf=open('../media/data.html','r',encoding="utf-8")
       htmlcont=htmlf.read()
       send_mail(mailto_list,"实时监控预警",htmlcont)
 
